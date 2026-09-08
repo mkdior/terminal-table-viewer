9bc8f6aa8d75850adc69d76d05e553c87fa18945
mkdior <h@mza.jp>
2026-09-08 17:10:53 +0900

    ~ -- bump version to `v0.23.2`

--- cmd/ttv/main.go
+++ cmd/ttv/main.go
@@ -5,7 +5,7 @@ import "github.com/mkdior/terminal-table-viewer/internal/app"
 
 // version is the release version; overridden at build time with
 // -ldflags "-X main.version=...".
-var version = "0.23.1"
+var version = "0.23.2"
 
 func main() {
 	app.Execute(version)
