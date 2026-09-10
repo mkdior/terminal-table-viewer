60353e483c0a42474006355f7bb535b1ae8c885e
mkdior <h@mza.jp>
2026-09-10 13:30:14 +0900

    ~ -- bump version to `v0.25.0-dev.3`

--- cmd/ttv/main.go
+++ cmd/ttv/main.go
@@ -5,7 +5,7 @@ import "github.com/mkdior/terminal-table-viewer/internal/app"
 
 // version is the release version; overridden at build time with
 // -ldflags "-X main.version=...".
-var version = "0.25.0-dev.2"
+var version = "0.25.0-dev.3"
 
 func main() {
 	app.Execute(version)
